b97633d89190406933989d90fdb8b647b06ae212
Jackson Parsons <5050183+J-Parsons@users.noreply.github.com>
2018-12-07 09:59:17 -0700

    version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='uledger',
-    version='0.2.2',
+    version='0.2.3',
     description='Python SDK for the ULedger API',
     license='Apache License 2.0',
     author='Jackson Parsons',
